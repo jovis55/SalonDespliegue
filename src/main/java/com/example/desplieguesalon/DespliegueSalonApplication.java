28e63e703195b8111d48da582b933b98fb9a5217
johana <johanapaolapalacio@gmail.com>
2024-03-30 15:26:40 -0500

    Primera configuracion

---
+++ src/main/java/com/example/desplieguesalon/DespliegueSalonApplication.java
@@ -0,0 +1,22 @@
+package com.example.desplieguesalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class DespliegueSalonApplication {
+
+
+    @GetMapping("/mensaje")
+    public String mensaje(){
+        return "Bienvenido";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DespliegueSalonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desplieguesalon/DespliegueSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desplieguesalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespliegueSalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
