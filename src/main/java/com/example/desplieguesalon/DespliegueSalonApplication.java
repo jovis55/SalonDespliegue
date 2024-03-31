be2226be0f1984648718377d3c2c7295386d2134
johana <johanapaolapalacio@gmail.com>
2024-03-30 22:04:30 -0500

    commit prueba2

--- src/main/java/com/example/desplieguesalon/DespliegueSalonApplication.java
+++ src/main/java/com/example/desplieguesalon/DespliegueSalonApplication.java
@@ -13,7 +13,7 @@ public class DespliegueSalonApplication {
 
     @GetMapping("/mensaje")
     public String mensaje(){
-        return "Bienvenido juancho";
+        return "Bienvenido juan";
     }
     public static void main(String[] args) {
         SpringApplication.run(DespliegueSalonApplication.class, args);
